cb61458404c4f10e826abd5e99ef8c60cad031d1
MarcoBaygorria <arrancar1991@live.cl>
2022-06-05 16:27:23 -1100

    Instalacion de tailwind.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Encabezado from './componentes/compartidos/Encabezado';
+import Pie from './componentes/compartidos/Pie';
+import Principal from './componentes/compartidos/Principal';
 
 function App() {
   return (
     <div className="App">
+      <Encabezado></Encabezado>
+      <Principal></Principal>
+      <Pie></Pie>
     </div>
   );
 }
---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer className="text-red-500">
+          <small>Desarrollado y diseñado por Marco Baygorria Saavedra, 2022 Todos los derechos reservados.</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
